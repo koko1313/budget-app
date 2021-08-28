cc6895dd2c03a0f6cb6f929b87f78212e212d5e6
koko1313 <kaloyanvellichkov@gmail.com>
2021-08-28 23:13:20 +0300

    api setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
+app.use(bodyParser());
+
+// ############################################################
+
+app.post("/inputBudget", (req, res) => {
+    // TODO
+});
+
+app.get("/getBalance", (req, res) => {
+    // TODO
+});
+
+app.post("/expense", () => {
+    // TODO
+});
+
+app.post("/editExpense", () => {
+    // TODO
+})
+
+add.post("/removeExpense", () => {
+    // TODO
+});
+
+// ############################################################
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
